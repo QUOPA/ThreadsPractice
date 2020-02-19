59144ee3e0557b2f972700dcf28005ec4f70eac7
Woojin Park <woojjinpark@gmail.com>
2020-02-20 03:14:52 +0900

    Executing Three Threads

---
+++ src/ThreadPractice/main.cpp
@@ -0,0 +1,33 @@
+﻿
+//#include "pch.h"
+
+
+#include <iostream>
+#include <thread>
+#include <string>
+#include <sstream>
+
+int main()
+{
+	auto FnToCall = [](std::string FnName, int Num) {
+		
+		std::ostringstream tmpstr2;
+		tmpstr2 << "<" << FnName << "is Called>\n";
+		std::cout << tmpstr2.str();
+		for (int i = 0; i < Num; i++)
+		{
+			std::ostringstream tmpstr;
+			tmpstr << FnName << " : (" << i << "/" << Num << ")\n";
+			std::cout << tmpstr.str();
+		}
+	};
+	
+	std::thread tmpThread(FnToCall, "AAAA", 10);
+	std::thread tmpThread2(FnToCall, "BBBB", 10);
+	std::thread tmpThread3(FnToCall, "CCCC", 10);
+	
+	tmpThread.join();
+	tmpThread2.join();
+	tmpThread3.join();
+     
+}
